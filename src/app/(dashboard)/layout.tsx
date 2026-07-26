0afff89db7dced0e9a741478e68ac3d2105a96b7
codiceps <rehandimas4444@gmail.com>
2026-07-26 22:11:45 +0700

    fix: prevent desktop padding top override

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -15,7 +15,7 @@ export default async function DashboardLayout({
     <div className="relative min-h-screen bg-ink">
       <Sidebar user={session?.user} />
       <TopNavbar />
-      <main className="lg:ml-60 pt-16 pb-20 lg:pb-0 min-h-screen p-4 lg:p-6 flex flex-col gap-4">
+      <main className="lg:ml-60 px-4 pt-24 pb-20 lg:px-6 lg:pt-24 lg:pb-6 min-h-screen flex flex-col gap-4">
         {children}
       </main>
     </div>
